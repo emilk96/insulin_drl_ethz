9973c7bf121493766bae7b7360e78fda922976c9
emilk96 <49405483+emilk96@users.noreply.github.com>
2021-10-21 16:45:14 +0200

    Update T1DEKTF.py

--- sim_transfer/sac/T1DEKTF.py
+++ sim_transfer/sac/T1DEKTF.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+
 from tf_agents.environments import py_environment
 from tf_agents.environments import tf_environment
 from tf_agents.environments import tf_py_environment
@@ -9,7 +10,6 @@ from tf_agents.environments import wrappers
 from tf_agents.environments import suite_gym
 from tf_agents.trajectories import time_step as ts
 
-
 from simglucose.simulation.env import T1DSimEnv as _T1DSimEnv
 from simglucose.patient.t1dpatient import T1DPatient
 from simglucose.sensor.cgm import CGMSensor
@@ -17,6 +17,7 @@ from simglucose.actuator.pump import InsulinPump
 from simglucose.simulation.scenario_gen import RandomScenario
 from simglucose.simulation.scenario import CustomScenario
 from simglucose.controller.base import Action
+
 import numpy as np
 import pkg_resources
 import gym
@@ -25,34 +26,43 @@ from gym.utils import seeding
 from datetime import datetime
 import matplotlib.pyplot as plt
 
+#Patient Parameters for 10 children, adolescents and adults
 PATIENT_PARA_FILE = pkg_resources.resource_filename(
     'simglucose', 'params/vpatient_params.csv')
 
-#Formula: BW*u2ss/6000=BR
 
-#Adolescent1: BW 68.706, u2ss 1.2169            -> BR = 0.014 ->max = 7 * BR = 0.098 #0.09
-#Adult7: BW 91.229, u2ss 1.503                  -> BR = 0.02285 -> max = 7 *BR = 0.1599 #0.1?
-#Adolescent9: BW 43.885, u2ss 1.38186522046     -> BR = 0.0101 -> max = 0.0708 
-#Child 4: BW 35.5165043, u2ss 1.38610897835     -> BR = 8.204*10e-3 -> 0.0574
-#Child7 = 0.0574
+
+
+##---------------------######################
+# Type 1 Diabetes Environment 
 
 class T1DEKTF(py_environment.PyEnvironment):
-    """Custom Environment that follows TF interface""" 
+    """
+        Custom Environment that follows TF-agents interface
+    """ 
+    
     SENSOR_HARDWARE = 'Dexcom'
     INSULIN_PUMP_HARDWARE = 'Insulet'
+    
 
     def __init__(self, patient_name=None, reward_function=None, seed=None):
+        """
+            New simulator instance, reset simulator
+        """
+        
+        state_length = 120 #in minutes, passed to rnn
+        
         self._observation_spec = array_spec.BoundedArraySpec(
-            shape=(120,), dtype=np.float32, minimum=0, maximum=np.inf, name='simulator')
+            shape=(state_length,), dtype=np.float32, minimum=0, maximum=np.inf, name='simulator')
         self._action_spec = array_spec.BoundedArraySpec(
             shape=(1,), dtype=np.float32, minimum=0, maximum=0.09, name='insulin')
-        self._state =  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-        #self._state =  [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
+        
+        self._state =  [0] * state_length
         self._state = np.asarray(self._state).reshape(self._observation_spec.shape)
         self._episode_ended = False
 
         if patient_name is None:
-            patient_name = 'adolescent#001' #Change patient here 
+            patient_name = 'adolescent#001' #Default patient if none selected 
         self.patient_name = patient_name
         self.reward_function = reward_function
         self.np_random, _ = seeding.np_random(seed=seed)
@@ -65,6 +75,10 @@ class T1DEKTF(py_environment.PyEnvironment):
         return self._observation_spec
 
     def _step(self, action):
+        """ 
+            New action affects the simulator, first check if simultor is still running
+        """
+        
         if self._episode_ended:
             return self.reset()
         else:
@@ -75,9 +89,10 @@ class T1DEKTF(py_environment.PyEnvironment):
             obs, reward, done, _ = self.simulator.step(act)
         else:
             obs, reward, done, _ = self.simulator.step(act, reward_fun=self.reward_function)
+            
         obs = obs[0]
-        # self._state = np.delete(self._state, 0)
-        # self._state = np.insert(self._state, self._observation_spec.shape[0]-1, obs, axis=0)
+        
+        #Insert new observation into state
         self._state = np.delete(self._state, 0, axis=0)
         self._state = np.insert(self._state, self._observation_spec.shape[0]-1, obs, axis=0)
         self._state = np.reshape(self._state, self._observation_spec.shape)
@@ -91,14 +106,19 @@ class T1DEKTF(py_environment.PyEnvironment):
 
 
     def seed(self, seed=None):
+        """
+            Seed function for new simulator
+        """
+        
         self.np_random, seed1 = seeding.np_random(seed=seed)
         self.env, seed2, seed3, seed4 = self.create_simulator_from_random_state()
         return [seed1, seed2, seed3, seed4]
 
     def create_simulator_from_random_state(self):
-        # Derive a random seed. This gets passed as a uint, but gets
-        # checked as an int elsewhere, so we need to keep it below
-        # 2**31.
+        """
+            Seed function for new simulator
+        """ 
+        
         seed2 = seeding.hash_seed(self.np_random.randint(0, 1000)) % 2**31
         seed3 = seeding.hash_seed(seed2 + 1) % 2**31
         seed4 = seeding.hash_seed(seed3 + 1) % 2**31
